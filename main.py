6f0fd750f76b01ccfa1f6e928f32f70306d02924
Norbert Tasarz <tasarz_norbert@wp.pl>
2024-10-24 20:33:23 +0200

    reszta programu done

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-
+import random
 class Bot:
     def __init__(self):
         options = webdriver.ChromeOptions()
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
 
     def OpenForm(self, form_url):
         self.driver.get(form_url)
@@ -18,31 +22,42 @@ class Bot:
         return div_z_pytaniami.find_elements(By.CLASS_NAME, "Qr7Oae")
 
     def WybierzOpcjeRadioButton(self, pytanie, nr_opcji):
-        btns = pytanie.find_elements(By.CSS_SELECTOR, ".d7L4fc.bJNwt.FXLARc.aomaEc.ECvBRb")
-        btns[nr_opcji].click()
+        btns = pytanie.find_elements(By.CSS_SELECTOR, '[role="radio"][aria-label]')
+        if 0 <= nr_opcji < len(btns):
+            btns[nr_opcji].click()
 
     def WybierzOpcjeCheckButton(self, pytanie, nr_opcji):
-        btns = pytanie.find_elements(By.CSS_SELECTOR, ".uVccjd.aiSeRd.FXLARc.wGQFbe.BJHAP.oLlshd")
-        btns[nr_opcji].click()
+        btns = pytanie.find_elements(By.CSS_SELECTOR, '[role="checkbox"][aria-label]')
+        if 0 <= nr_opcji < len(btns):
+            btns[nr_opcji].click()
 
+    def WybierzOpcjeSkali(self, pytanie, nr_opcji):
+        btns = WebDriverWait(pytanie, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".Od2TWd.hYsg7c"))
+        )
+        # Find all elements matching the selector
+        buttons = pytanie.find_elements(By.CSS_SELECTOR, ".Od2TWd.hYsg7c")
+        
+        # Ensure the option index is valid
+        if nr_opcji < len(buttons):
+            buttons[nr_opcji].click()
+        
     def Prześlij(self):
         submit_button = self.driver.find_element(By.XPATH, "//div[@role='button']//span[text()='Prześlij']")
         submit_button.click()
 
-    def DoTheThing(self):
-        self.driver.get("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-        time.sleep(10)
-
 if __name__ == "__main__":
     bot = Bot()
-    DoTheThing()
-    bot.OpenForm("https://docs.google.com/forms/d/e/1FAIpQLSctNmsjLW082da48bVWE2Euy7l5C1zfRi4-oTh59e3a22uDFA/viewform")
-    pytania = bot.GetPytania()
-
-    if pytania:
-        bot.WybierzOpcjeRadioButton(pytania[0], 0)
-        bot.WybierzOpcjeCheckButton(pytania[1], 3)
-        # itd...
-    
-    input("test")
-    bot.Prześlij()  
+    while(True):
+        bot.OpenForm("https://docs.google.com/forms/d/e/1FAIpQLSdQI-VK9i91942aHII7O_nZ6QxDx14ekVBnz6V5FfWjYFsGbQ/viewform")
+        pytania = bot.GetPytania()
+        if pytania: 
+            bot.WybierzOpcjeRadioButton(pytania[0], (random.randint(0, 4))) # Jak często podróżujesz pociągiem?
+            bot.WybierzOpcjeRadioButton(pytania[1], (random.randint(0, 5))) # Ile masz lat?
+            bot.WybierzOpcjeRadioButton(pytania[2], (random.randint(0, 4))) # W jakim celu najczęściej podróżujesz?
+            bot.WybierzOpcjeCheckButton(pytania[3], (random.randint(0, 4))) # Z którym z wymienionych problemów spotkałeś się podczas podróży pociągiem?
+            bot.WybierzOpcjeRadioButton(pytania[4], (random.randint(0, 3))) # Który problem jest według ciebie najpoważniejszy?
+            bot.WybierzOpcjeSkali(pytania[5], (random.randint(0, 9))) # W skali od 1 do 10, jak bardzo wcześniej zaznaczone problemy wpływają na twoją podróż? (1- nie wpływa, 10 - uniemożliwia podróż)
+            bot.WybierzOpcjeSkali(pytania[6], (random.randint(0, 9))) # Jak oceniasz ogólny poziom bezpieczeństwa podczas podróży pociągiem?  (1 - bardzo niebezpiecznie, 10 - bardzo bezpiecznie)
+            bot.WybierzOpcjeCheckButton(pytania[7], (random.randint(0, 1))) # Czy byłbyś zainteresowany którymś z tych rozwiązań?
+            bot.Prześlij()
